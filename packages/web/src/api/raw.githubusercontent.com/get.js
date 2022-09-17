2071b5872675b9c4d1d6b58183f666066529ad08
José Diogo <jdcdiniz@gmail.com>
2022-09-17 17:52:31 -0300

    Corrigindo get

--- packages/web/src/api/raw.githubusercontent.com/get.js
+++ packages/web/src/api/raw.githubusercontent.com/get.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const RAW_GITHUB_CONTENT =
     'https://raw.githubusercontent.com';
 const REPOS_STORAGE_JSON_CONHECIMENTO =
-    '/zedeogo/storage/master/zedeogo.github.io/json/conhecimento.json';
+    '/zedeogo/zedeogo.github.io/master/packages/data/storage/json/conhecimento.json';
 
 const api = axios.create({
     baseURL: RAW_GITHUB_CONTENT
