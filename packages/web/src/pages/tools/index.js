1ba9605a3dd4fb4c272b1cb8fd799fd90bc8f8f6
José Diniz <jdcdiniz@gmail.com>
2022-09-26 11:51:40 -0300

    Começo do tools

--- packages/web/src/pages/tools/index.js
+++ packages/web/src/pages/tools/index.js
@@ -3,7 +3,38 @@ import StyledTools from './styles';
 
 const Tools = _ => {
     return (
-        <StyledTools></StyledTools>
+        <StyledTools>
+            <div className="tool_init-space" />
+            <div className="w100 tools__h2-line relative fit"><h2 className='capitalize cFFFFFF b000000 fit'>texto</h2></div>
+            <div className="container-column w100">
+                <ul>
+                    <li>
+                        <div>
+                            <h3>Inverter Texto</h3>
+                            <p>Gera um texto invertido;</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>Inverter Texto</h3>
+                            <p>Gera um texto invertido;</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>Inverter Texto</h3>
+                            <p>Gera um texto invertido;</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>Inverter Texto</h3>
+                            <p>Gera um texto invertido;</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </StyledTools>
     )
 }
 
--- packages/web/src/pages/tools/styles.js
+++ packages/web/src/pages/tools/styles.js
@@ -1,6 +1,16 @@
 import styled from 'styled-components';
 
 const StyledTools = styled.div`
+    padding: 2rem;
+    .tool_init-space {
+        height: 8rem;
+    }
+    h2 {
+        padding: .1rem 1rem;
+    }
+    .tools__h2-line {
+        border-bottom: 4px solid #000;
+    }
 `;
 
 export default StyledTools;
